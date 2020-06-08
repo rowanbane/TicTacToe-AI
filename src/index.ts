baa8b26fe5a900f4d7f8d6a9cfc161243965a7d7
unknown <rowanspenc@gmail.com>
2020-06-08 09:49:20 +0100

    Cleaned up comments

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import "gsap";
 import gsap from "gsap";
 import TicTacToe from "./Game/TicTacToe";
 
-console.log("what's up guys, Rowan here coming at you live with another");
 window["initGame"] = () => {
     const renderer = new PIXI.Renderer({
         width: 800,
@@ -18,10 +17,4 @@ window["initGame"] = () => {
     });
 
     stage.addChild(new TicTacToe());
-    // const test = new PIXI.Graphics();
-    // test.beginFill(0xff0000);
-    // test.drawCircle(0, 200, 20);
-    // stage.addChild(test);
-
-    // gsap.to(test, { x: 500, duration: 50 });
 };
